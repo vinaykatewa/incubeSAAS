943de97a5490618e209e5cb1fb80598f4b6b10fa
vinaykatewa <imvinaykatewa@gmail.com>
2023-12-04 21:44:14 +0530

    code

--- lib/firebase/firebase.dart
+++ lib/firebase/firebase.dart
@@ -5,49 +5,6 @@ import 'dart:io';
 import 'dart:typed_data';
 
 class FirebaseClass {
-  // Future<UserCredential?> signup(
-  // String accelerator_Name,
-  // String user_Name,
-  // String email,
-  // String website_Link,
-  // String password,
-  //     File imageFile) async {
-  //   try {
-  //     final credential = await FirebaseAuth.instance
-  //         .createUserWithEmailAndPassword(email: email, password: password);
-  //     final storageRef = FirebaseStorage.instance
-  //         .ref()
-  //         .child('user_image')
-  //         .child('${credential.user!.uid}.jpg');
-  //     await storageRef.putFile(imageFile);
-  //     final imageUrl = await storageRef.getDownloadURL();
-
-  //     final collectionRef =
-  //         FirebaseFirestore.instance.collection('${credential.user!.uid}');
-  //     final docSnapshot = await collectionRef.doc('Fields').get();
-  //     if (!docSnapshot.exists) {
-  //       await collectionRef.doc('Fields').set({
-  // 'Accelerator_Name': accelerator_Name,
-  // 'User_Name': user_Name,
-  // 'Email': email,
-  // 'Website_Link': website_Link,
-  // 'password': password,
-  // 'imageUrl': imageUrl,
-  //       });
-  //     }
-  //     return credential;
-  //   } on FirebaseAuthException catch (e) {
-  //     if (e.code == 'weak-password') {
-  //       print('The password provided is too weak.');
-  //     } else if (e.code == 'email-already-in-use') {
-  //       print('The account already exists for that email.');
-  //     }
-  //     return null;
-  //   } catch (e) {
-  //     print(e);
-  //     return null;
-  //   }
-  // }
   Future<UserCredential?> signup(
       String accelerator_Name,
       String user_Name,
